03f0f28eda2e510ab0ce8a11a2a84355e3cc6687
Nikolay <Nikolay89Veselinov.com>
2021-11-25 13:27:00 +0200

    Created category MPTTModel

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to="images")
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='children')
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+
