4936ccd52500c6c51024019f7932d81283114171
Nikolay <Nikolay89Veselinov.com>
2021-11-25 15:48:26 +0200

    Created categories admin and draggable option

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from mptt.admin import DraggableMPTTAdmin
 
-# Register your models here.
+from .models import Category
+
+
+@admin.register(Category)
+class CategoryDraggableMPTTAdmin(DraggableMPTTAdmin):
+    list_display = ('tree_actions', 'move_element')
+    list_display_links = ('move_element',)
+
+    def move_element(self, instance):
+        return format_html(
+            '<div style="text-indent:{}px">{}</div>',
+            instance._mpttfield('level') * self.mptt_level_indent,
+            instance.name,
+        )
+
+    move_element.short_description = 'move element'
+    
