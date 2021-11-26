3d584b79aab299ad2063bcb0b22cd76618e2ec1c
Nikolay <Nikolay89Veselinov.com>
2021-11-26 15:34:54 +0200

    Set path to templates folder and media setup

--- homework/settings.py
+++ homework/settings.py
@@ -1,3 +1,7 @@
+import os
+
+DATA_DIR = os.path.dirname(os.path.dirname(__file__))
+
 """
 Django settings for homework project.
 
@@ -56,7 +60,7 @@ ROOT_URLCONF = 'homework.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
